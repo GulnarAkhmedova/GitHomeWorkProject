46a0fcaed0b7d3c4af5b4cefe16985e121b37e01
elnar <elnarasabira@gmal.com>
2022-05-14 13:26:08 -0700

    check up

--- src/com/syntax/HWTask01.java
+++ src/com/syntax/HWTask01.java
@@ -4,8 +4,9 @@ public class HWTask01 {
     public static void main(String[] args) {
         int n=100;
         for (int i = 0; i < n; i++) {
-            System.out.print(i+ " ");
+            System.out.println(i+ " ");
 
         }
+        System.out.println("check up");
     }
 }
