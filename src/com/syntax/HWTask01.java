f87afb5ff991427af443df917f2751e547a52a69
elnar <elnarasabira@gmal.com>
2022-05-15 20:05:02 -0700

    3rd commit

--- src/com/syntax/HWTask01.java
+++ src/com/syntax/HWTask01.java
@@ -2,7 +2,7 @@ package com.syntax;
 
 public class HWTask01 {
     public static void main(String[] args) {
-        int n=100;
+        int n=50;
         for (int i = 0; i < n; i++) {
             System.out.println(i+ " ");
 
